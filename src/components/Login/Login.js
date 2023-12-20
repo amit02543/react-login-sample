d35ef28aa44cac6dd249a94be0843a5088719301
Anish <simplifypib@gmail.com>
2023-12-21 02:38:11 +0530

    added axios api call

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
 
 import Card from '../UI/Card/Card';
 
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
-const Login = () => {
+const Login = (props) => {
 
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredPassword, setEnteredPassword] = useState('');
@@ -13,6 +14,46 @@ const Login = () => {
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
 
+
+    async function validateUserLogin() {
+        setIsSubmitting(true);
+
+        const headers = { 
+            'Content-Type': 'application/json'
+        };
+        
+        const response = await axios.post(
+            'http://localhost:8080/login', 
+            JSON.stringify({username: enteredUsername, password: enteredPassword}),
+            { headers }
+        )
+        .then(res => {
+            console.log(res);
+            setIsSubmitting(false);
+        })
+        .catch(err => {
+            console.log(err.response.data);
+            console.log(err.response.data.message);
+            setErrorMessage(err.response.data.message);
+            console.log(errorMessage);
+            setIsSubmitting(false);
+        });
+    }
+
+
+    // useEffect(() => {
+
+    //     // const identifier = setTimeout(() => {
+    //     //   validateUserLogin();
+    //     // }, 500);
+    
+    //     // return () => { clearTimeout(identifier) }; 
+    //     console.log('use effect error message: ' + errorMessage);   
+    //     if(errorMessage) {
+    //         validateUserLogin();
+    //     }
+    // }, [errorMessage]);
+    
 
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
@@ -30,11 +71,64 @@ const Login = () => {
             enteredUsername.trim().length > 0 && event.target.value.trim().length >= 6
         );
     };
-    
+
+
 
     const submitHandler = async (event) => {
         event.preventDefault();
+
+        validateUserLogin();
+
+        // const response = await fetch('http://localhost:8080/login', {
+        //     method: 'POST',
+        //     headers: {
+        //       'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({username: enteredUsername, password: enteredPassword})
+        // }).then(res => {
+        //     console.log(res);
+        // }).catch(err => {
+        //     console.log(err);
+        // });
+
+        // console.log(response);
+        // console.log(response.ok);
+
+        // if(!response.ok) {
+        //     console.log('setting error message: ' + response.message);
+        //     setErrorMessage(response.message);
+        // }
+
     };
+
+    // const submitHandler = useCallback( async event => {
+    //     event.preventDefault();
+        
+
+    //     setIsSubmitting(true);
+
+    //     const headers = { 
+    //         'Content-Type': 'application/json'
+    //     };
+        
+    //     const response = await axios.post(
+    //         'http://localhost:8080/login', 
+    //         JSON.stringify({username: enteredUsername, password: enteredPassword}),
+    //         { headers }
+    //     )
+    //     .then(res => {
+    //         console.log(res);
+    //         setIsSubmitting(false);
+    //     })
+    //     .catch(err => {
+    //         console.log(err.response.data);
+    //         console.log(err.response.data.message);
+    //         setErrorMessage(err.response.data.message);
+    //         console.log(errorMessage);
+    //         setIsSubmitting(false);
+    //     });
+
+    // }, [errorMessage])
 
 
     return (
