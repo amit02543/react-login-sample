49652e916f424b1d1018616933229f730e7c0848
Amit Kumar <amit02543@yahoo.com>
2023-12-21 21:21:22 +0530

    fixed navigation links

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,29 +1,77 @@
-import React from 'react';
+import { Form, NavLink, useRouteLoaderData } from 'react-router-dom';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
-  return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Collection</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Profile</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
+
+    const token = useRouteLoaderData('root');
+
+    return (
+        <nav className={classes.nav}>
+            <ul>
+                <li>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? classes.active : undefined
+                        }
+                        end
+                        >
+                        Home
+                    </NavLink>
+                </li>
+                {!token && (
+                    <li>
+                        <NavLink
+                            to="/login"
+                            className={({ isActive }) =>
+                                isActive ? classes.active : undefined
+                            }
+                            end
+                            >
+                            Login
+                        </NavLink>
+                    </li>
+                )}
+                {!token && (
+                    <li>
+                        <NavLink
+                        to="/register"
+                        className={({ isActive }) =>
+                            isActive ? classes.active : undefined
+                        }
+                        end
+                        >
+                        Register
+                        </NavLink>
+                    </li>
+                )}
+                {/* {!props.isLoggedIn && (
+                <li>
+                    <a href="/register">Register</a>
+                </li>
+                )} */}
+                {token && (
+                <li>
+                    <a href="/collection">Collection</a>
+                </li>
+                )}
+                {token && (
+                <li>
+                    <a href="/profile">Profile</a>
+                </li>
+                )}
+                {token && (
+                    <li>
+                        {/* <button onClick={props.onLogout}>Logout</button> */}
+                        <Form action="/logout" method="post">
+                            <button>Logout</button>
+                        </Form>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    );
 };
 
 export default Navigation;
