4bba9c2370483cb3986d71f48faefa16fea9b02e
Amit Kumar <amit02543@yahoo.com>
2024-01-06 17:48:48 +0530

    added random music changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
 import Card from '../UI/Card/Card';
+import Track from '../Search/Track';
+
 import classes from './Home.module.css';
 
-const Home = (props) => {
+const Home = ({ data }) => {
 
   const user = localStorage.getItem('user');
+  console.log('Home random music: ', data);
+
+  const tracks = data.map(track => <Track track={track} /> );
 
   return (
     <Card className={classes.home}>
-      <h1>Welcome{user && ` back ${user}`}!</h1>
+      {/* <h1>Welcome{user && ` back ${user}`}!</h1> */}
+      <div>{tracks}</div>
     </Card>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,53 @@
+import { Suspense } from 'react';
+import { Await, defer, json, useLoaderData } from 'react-router-dom';
+
 import Home from '../components/Home/Home';
 import PageContent from '../components/PageContent/PageContent';
 
+
 function HomePage() {
+
+    const { randomMusic } = useLoaderData();
+
     return (
-        <PageContent title="Welcome!">
-            <p>Browse all our amazing songs collection!</p>
-            <Home />
-        </PageContent>
+        <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+            <Await resolve={randomMusic}>
+                {
+                    response => (
+                        <PageContent title="Welcome!">
+                            <p>Browse all our amazing songs collection!</p>
+                            <Home data={response} />
+                        </PageContent>
+                    )
+                }
+            </Await>
+        </Suspense>
     );
 }
 
 export default HomePage;
+
+
+async function loadRandomMusic() {
+
+    const response = await fetch('http://localhost:8080/spotify/random');
+
+    if(!response.ok) {
+        throw json(
+            { message: 'Could not fetch random music.'},
+            { status: 500 }
+        );
+    } 
+    
+    const resData = await response.json();
+    console.log(resData.tracks)
+
+    return resData.tracks;
+};
+
+
+export function loader() {
+    return defer({
+        randomMusic: loadRandomMusic()
+    });
+};
